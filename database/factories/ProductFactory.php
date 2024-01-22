91e80cf321859b8870aa0d73134e6e957f12640b
i3b77 <abodomar1212@gmail.com>
2024-01-22 18:44:28 +0300

    updating product and categories m

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+            return [
+                'product_name' => \Faker\Factory::create()->unique()->word,
+                'brand' => \Faker\Factory::create()->unique()->company,
+                'description' => \Faker\Factory::create()->sentence(),
+                'quantity' => \Faker\Factory::create()->randomNumber(),
+                'product_price' => \Faker\Factory::create()->randomFloat(2, 0, 1000),
+                'color' => \Faker\Factory::create()->optional()->colorName,
+                'created_at' => now(),
+                'updated_at' => now(),
+                'deleted_at' => null,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
+use App\Models\Product;
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,13 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(1000)->create()->each(function($user){
-            $user->addresses()->saveMany(\App\Models\UserAddress::factory(rand(1,3))->make());
-        });
+        //Product::factory(5)->create();
+        //User::factory(1000)->create()->each(function($user){
+            //$user->addresses()->saveMany(\App\Models\UserAddress::factory(rand(1,3))->make());
+            //Category::factory(5)->create()->each(function ($categories) {
+              //  $categories->products()->saveMany(Product::factory(10)->make());
+             Category::factory(5)->create()->each(function ($category) {
+                Product::factory(10)->create(['category_id' => $category->id]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-    }
+    });
+
+}
 }
