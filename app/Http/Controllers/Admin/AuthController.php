c233d1689026d5cfff4266b10e39efcac2496f9b
jadoun91 <jadoun91@gmail.com>
2024-02-03 18:40:59 +0530

    admin theme integration

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -7,13 +7,8 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //index
-    public function index(){
-        return "Welcome to admin panel";
-    }
-
     //login
     public function login(){
-        return "Login";
+        return view('admin.login');
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\Admin\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [AuthController::class, 'index'])->name('admin.index');
+Route::get('/', [AuthController::class, 'login'])->name('admin.login');
